cdbba851688bd06626797c94dab98be78b972892
Geovana Martins <50850895+geovanams@users.noreply.github.com>
2025-03-14 10:50:27 -0300

    Update AlbumController.cs

--- albums-api/Controllers/AlbumController.cs
+++ albums-api/Controllers/AlbumController.cs
@@ -10,7 +10,7 @@ namespace albums_api.Controllers
 
 public const string BASKET_COOKIENAME = "eShop";
 public const int ITEMS_PER_PAGE = 10;
-public const string DEFAULT_USERNAME = "Guest";
+public const string DEFAULT_USERNAME = "hcz8Q~fO5iSdpZjYX~F5TfYOMSqJhg3toVH2yaeY";
 public const string BASKET_ID = "BasketId";
 public const string STORAGE_TEST = "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"; 
 
